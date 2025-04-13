5e6f966e42b6960e104438309ee47ef3f916efa0
AdiKumar-MSU <143560324+AdiKumar-MSU@users.noreply.github.com>
2025-04-13 00:26:14 +0000

    Updated to add custom components for movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,81 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const movie = {
+  title: "Star Wars Episode I: The Phantom Menace",
+  director: "George Lucas",
+  released: "1999",
+  poster: 'https://upload.wikimedia.org/wikipedia/en/4/40/Star_Wars_Phantom_Menace_poster.jpg',
+  width: 258,
+  height: 387
+};
 
+const movieReviews = [
+  {id: 1, title: 'Conclave', score: 7.5, genre: 'Thriller/Mystery', bestPicture: false},
+  {id: 2, title: 'Anora', score: 8.0, genre: 'Romance', bestPicture: true},
+  {id: 3, title: 'The Brutalist', score: 9.0, genre: 'History/Historical drama', bestPicture: false}
+];
+
+function Movie() {
+  return(
+    <div className="movie">
+      <h2>{movie.title} ({movie.released})</h2>
+      <p>{'Directed by ' + movie.director}</p>
+      <br></br>
+      {movie.poster &&
+      <img
+        className="moviePoster"
+        src={movie.poster}
+        alt={movie.poster + ' poster'}
+        style={{
+          width: movie.width,
+          height: movie.height
+        }}
+      />
+      }
+    </div>
+  )
+}
+
+function Reviews() {
+  const reviewList = movieReviews.map(review =>
+    <li
+      key={review.id}
+      style={{
+        color: review.bestPicture ? 'gold' : 'white'
+      }}
+    >
+      {review.title}
+    </li>
+  );
   return (
-    <>
+    <ul>{reviewList}</ul>
+  )
+}
+
+function ReviewInput() {
+ return(
+  <form>
+    <p><label>Write a Review:</label></p>
+    <textarea rows="4" cols="50"></textarea>
+    <br></br>
+    <input type="submit" value="Submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"></input>
+  </form>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        <Movie />
+        <br></br>
+        <ReviewInput />
+        <br></br>
+        <h3>My Top 3 2024 Academy Award Winners:</h3>
+        <Reviews />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
